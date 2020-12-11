e3f0815d942bc9617c6bd600bcbdbbdf021a6799
nikhil <nikhil.tyagi62@gmail.com>
2020-12-11 15:47:10 +0530

    Levels and Sound effects added

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] float period;
+    [Range(0,1)] [SerializeField] float movementFactor;
+
+    Vector3 startingPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set movement factor
+        float cycles = Time.time / period; // grows from 0
+        const float tau = Mathf.PI * 2;
+
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
+    }
+}
--- Assets/Scripts/RocketController.cs
+++ Assets/Scripts/RocketController.cs
@@ -2,16 +2,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RocketController : MonoBehaviour
 {
     [SerializeField] float thrustForce = 2.0f;
     [SerializeField] float rotationStrength = 30f;
+
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip finishClip;
+    [SerializeField] AudioClip deathClip;
+
+    [SerializeField] ParticleSystem thrust;
+    [SerializeField] ParticleSystem finish;
+    [SerializeField] ParticleSystem death;
     // Start is called before the first frame update
 
-    Boolean dead = false;
     Rigidbody rigidBody;
     AudioSource audioSource;
+
+    enum State { Alive, Dying, Trancending }
+    State state = State.Alive;
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
@@ -21,46 +32,88 @@ public class RocketController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-            Thrusting();
-            Rotate();
+        if(state == State.Alive)
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Alive) { return; }
         switch(collision.gameObject.tag)
         {
             case "Friendly":
                 // do nothing
                 break;
+            case "Finish":
+                // Go to next level
+                FinishSequence();
+                break;
             default:
-                dead = true;
+                // restart this level.
+                DeathSequence();
                 break;
         }
     }
 
-    private void Rotate()
+    private void FinishSequence()
+    {
+        state = State.Trancending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(finishClip);
+        finish.Play();
+        Invoke("LoadNextScene", 1f);
+    }
+
+    private void DeathSequence()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(deathClip);
+        death.Play();
+        Invoke("LoadPrevScene", 1f);
+    }
+
+    private void LoadPrevScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void RespondToRotateInput()
     {
         rigidBody.freezeRotation = true;
-        if (Input.GetAxis("Horizontal") != 0 && !dead)
+        if (Input.GetAxis("Horizontal") != 0)
         {
             transform.Rotate(Vector3.back * Input.GetAxis("Horizontal") * Time.deltaTime * rotationStrength);
         }
         rigidBody.freezeRotation = false;
     }
 
-    private void Thrusting()
+    private void RespondToThrustInput()
     {
-        if (Input.GetKey(KeyCode.Space) && !dead)
+        if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up * thrustForce);
             if (!audioSource.isPlaying)
             {
-                audioSource.Play();
+                audioSource.PlayOneShot(mainEngine);
             }
-        }
-        else
+            if(!thrust.isPlaying)
+                thrust.Play();
+        } else
         {
             audioSource.Stop();
+            thrust.Stop();
         }
+
+        //if(Input.GetKeyUp(KeyCode.Space))
+
     }
 }
